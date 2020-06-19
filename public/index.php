861193315f387167cc2366fe1eb4cfa5b90478eb
Mike Iceman <it.trash.collector@gmail.com>
2020-06-19 22:48:09 +0200

    Let's rock

--- public/index.php
+++ public/index.php
@@ -2,3 +2,11 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use MikeIceman\Events\Emitters\EventEmitter;
+use MikeIceman\Events\Interfaces\EventEmitterInterface as Events;
+
+# Let's test it
+(new EventEmitter())->emitMultiple(
+    ['message' => 'Hello world!'],
+    Events::EVENT_SUBSCRIBE | Events::EVENT_ACTIVATE
+);
